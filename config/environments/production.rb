dedb77efa4826691f226b6199f583c679bdc9ebf
will <wsmi1289@gmail.com>
2018-11-25 17:42:02 -0500

    efgfgfvgrjk

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,18 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
+    ActionMailer::Base.smtp_settings = {
+      :address        => 'smtp.gmail.com',
+      :domain         => ENV.fetch['MAIL_DOMAIN'],
+      :port           => ENV.fetch['MAIL_PORT'],
+      :user_name      => ENV.fetch['MAIL_USER'],
+      :password       => ENV.fetch['MAIL_PASS'],
+      :authentication => :plain,
+      :enable_starttls_auto => true
+    }
+    # 587
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
