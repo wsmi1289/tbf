673ee439a5470b3008eb86c7f9a62262ba97d1fd
will <wsmi1289@gmail.com>
2018-11-25 16:09:37 -0500

    pg search fix

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2018_06_29_000329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
+  enable_extension "pg_trgm"
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
